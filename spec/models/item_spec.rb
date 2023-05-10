3b4f5b286758b93e60372f84a61789cc8af8514b
kentyan6 <ueken682@gmail.com>
2023-05-10 15:29:52 +0900

    商品出品機能　修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,6 +61,26 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it 'priceに半角数字以外が含まれていると登録できない' do
+        @item.price = '1111１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが300円未満では登録できない' do
+        @item.price = '250'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが9999999円を超えると登録できない' do
+        @item.price = '99999999'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
